7e9772cc0b434068fd17ab73dfcce88ee0abf0d9
Vishwajeet Kumar <vishwajeetku0107@gmail.com>
2026-03-31 03:48:07 +0530

    force redeploy

--- frontend/src/landingPage/signup/SignupPage.js
+++ frontend/src/landingPage/signup/SignupPage.js
@@ -136,6 +136,8 @@ function SignupPage() {
         } else {
             setErrors(newErrors);
         }
+        // add this anywhere
+        console.log("updated");
     };
 
     const inputStyle = {
